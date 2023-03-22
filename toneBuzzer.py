515bd8ce2c9e1d9e25318773737904dcf3267a98
Ichiky_Otsu <66952647+Brice66@users.noreply.github.com>
2023-03-22 21:08:41 +0100

    Update toneBuzzer.py

--- toneBuzzer.py
+++ toneBuzzer.py
@@ -3,45 +3,25 @@ from gpiozero.tones import Tone
 from time import sleep
 
 # Création d'une instance de TonalBuzzer sur la broche 21
-buzzer = TonalBuzzer(4)
+buzzer = TonalBuzzer(21)
 
 # Jouer les notes de la mélodie
-buzzer.play(Tone("C5"))
+buzzer.play(Tone("E4"))
 sleep(0.5)
-buzzer.play(Tone("C5"))
+buzzer.play(Tone("F#4"))
 sleep(0.5)
-buzzer.play(Tone("G5"))
+buzzer.play(Tone("G4"))
 sleep(0.5)
-buzzer.play(Tone("G5"))
+buzzer.play(Tone("A4"))
 sleep(0.5)
-buzzer.play(Tone("A5"))
+buzzer.play(Tone("B4"))
 sleep(0.5)
-buzzer.play(Tone("A5"))
+buzzer.play(Tone("A4"))
 sleep(0.5)
-buzzer.play(Tone("G5"))
-sleep(1)
-buzzer.play(Tone("F5"))
+buzzer.play(Tone("G4"))
 sleep(0.5)
-buzzer.play(Tone("F5"))
-sleep(0.5)
-buzzer.play(Tone("E5"))
-sleep(0.5)
-buzzer.play(Tone("E5"))
-sleep(0.5)
-buzzer.play(Tone("D5"))
-sleep(0.5)
-buzzer.play(Tone("D5"))
-sleep(0.5)
-buzzer.play(Tone("C5"))
+buzzer.play(Tone("F#4"))
 sleep(1)
 
 # Arrêter le buzzer
 buzzer.stop()
-
-
-
-
-    
-
-
-
